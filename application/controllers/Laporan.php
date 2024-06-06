c315dd70bd09448a8ce3c15b10d082925c26a036
nzedin <nzed215@gmail.com>
2024-06-07 04:44:56 +0800

    update 7/6

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -173,4 +173,36 @@ class Laporan extends CI_Controller {
         $this->load->view('templates/footer');
     }
 
+    public function student_transcript($warga)
+    {
+        $data['title'] = 'Transkrip Pelajar';
+        $data['warga'] = $warga;
+        $wargaID = $this->session->userdata('wargaID');
+        if ($warga == 'staff') {
+            $data['staff'] = $this->login_model->get_warga($wargaID, 'staff');
+        } else {
+            if ($this->login_model->ahli_kelab($wargaID) && $this->login_model->pengarah_program($wargaID)) {
+                $data['student_type'] = "both";
+            }
+            else if ($this->login_model->ahli_kelab($wargaID)) {
+                $data['student_type'] = "member";
+            }
+            else if ($this->login_model->pengarah_program($wargaID)){
+                $data['student_type'] = "programdirector";
+            } else {
+                $message = $this->session->set_flashdata('reminder', '<div class="text-small text-danger" role="alert">
+                Pelajar tidak dibenarkan akses! </div>');
+                redirect('login', $message);
+            }
+
+            $data['student'] = $this->login_model->get_warga($wargaID, 'student');
+
+        }
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidenav', $data);
+        $this->load->view('form/transkrip', $data);
+        $this->load->view('templates/footer');
+    }
+
+
 }
